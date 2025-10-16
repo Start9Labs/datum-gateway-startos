341c3360765489454e3e6ea2363bb56df706d534
Shadowy Super Coder <musashidisciple@proton.me>
2025-10-16 10:51:37 -0600

    bump version

--- startos/install/versions/index.ts
+++ startos/install/versions/index.ts
@@ -1,5 +1,6 @@
 import { v_0_4_0_1 } from './v0.4.0.1'
 import { v_0_4_0_2 } from './v0.4.0.2'
+import { v_0_4_0_3 } from './v0.4.0.3'
+export { v_0_4_0_4 as current } from './v0.4.0.4'
 
-export { v_0_4_0_3 as current } from './v0.4.0.3'
-export const other = [v_0_4_0_1, v_0_4_0_2]
+export const other = [v_0_4_0_1, v_0_4_0_2, v_0_4_0_3]

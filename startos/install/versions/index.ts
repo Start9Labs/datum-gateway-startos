d3c69783f06c0095fbdcc040007c1f25089f20be
Léo Haf <leo@haf.ovh>
2025-11-22 11:33:14 +0000

    update version

--- startos/install/versions/index.ts
+++ startos/install/versions/index.ts
@@ -1,6 +1,7 @@
 import { v_0_4_0_1 } from './v0.4.0.1'
 import { v_0_4_0_2 } from './v0.4.0.2'
 import { v_0_4_0_3 } from './v0.4.0.3'
-export { v_0_4_0_4 as current } from './v0.4.0.4'
+import { v_0_4_0_4 } from './v0.4.0.4'
+export { v_0_4_0_5 as current } from './v0.4.0.5'
 
-export const other = [v_0_4_0_1, v_0_4_0_2, v_0_4_0_3]
+export const other = [v_0_4_0_1, v_0_4_0_2, v_0_4_0_3, v_0_4_0_4]
---
+++ startos/install/versions/v0.4.0.5.ts
@@ -0,0 +1,10 @@
+import { VersionInfo } from '@start9labs/start-sdk'
+
+export const v_0_4_0_5 = VersionInfo.of({
+  version: '0.4.0:1-alpha.5',
+  releaseNotes: 'Revamped for StartOS 0.4.0',
+  migrations: {
+    up: async () => {},
+    down: async () => {},
+  }
+})

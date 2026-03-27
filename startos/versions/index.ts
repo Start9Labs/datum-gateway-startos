f37354fdf4ec547c65777b6a8f445dd63847a1d6
Léo Haf <leo@haf.ovh>
2026-03-27 11:18:53 +0000

    Fix version + commit for Bitcoin Knots

--- startos/versions/index.ts
+++ startos/versions/index.ts
@@ -1,7 +1,7 @@
 import { VersionGraph } from '@start9labs/start-sdk'
-import { v_0_4_1_2_b8 } from './v0.4.1.2.b8'
+import { v_0_4_1_2_b7 } from './v0.4.1.2.b7'
 
 export const versionGraph = VersionGraph.of({
-  current: v_0_4_1_2_b8,
+  current: v_0_4_1_2_b7,
   other: [],
 })
